e576cdd98e1a2cc94d4e267a3c1546678097e83f
unicodeveloper <prosperotemuyiwa@gmail.com>
2016-04-30 12:36:34 +0100

    Add something to be changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 7</div>
+                <div class="title">Laravel 7 is good</div>
             </div>
         </div>
     </body>
